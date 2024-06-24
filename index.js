0278a91af624a064f42d2dc38e904f150e636e75
ali.lubis <ali.lubis@quadrant-si.id>
2024-06-24 14:01:33 +0700

    add feature WDYT

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/app';
 import {name as appName} from './app.json';
+
+// Konfigurasi WDYR
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
