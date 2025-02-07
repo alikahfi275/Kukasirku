dfc4e6f5ebf20355f9528740e1a19c5cd25560b4
ali.lubis <ali.lubis@quadrant-si.id>
2025-02-07 11:08:52 +0700

    Fix: takeout modalsuccessfailed

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,14 +1,6 @@
 import ModalSucces from './ModalSucces';
 import CModal from './CModal';
 import CLoading from './CLoading';
-import CModalSuccesFailed from './CModalSuccesFailed';
 import {modalError, modalSuccess} from './CAlertNotifications';
 
-export {
-  ModalSucces,
-  CModal,
-  CLoading,
-  CModalSuccesFailed,
-  modalError,
-  modalSuccess,
-};
+export {ModalSucces, CModal, CLoading, modalError, modalSuccess};
